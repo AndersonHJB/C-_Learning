d102487da02c9e65e3b35ea6a71165a103604504
aiyc <1432803776@qq.com>
2021-07-11 12:15:04 +0800

    update

---
+++ ch2/ch2-HelloWord/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// main 函数：程序的入口，逻辑的起点，从这里开始。
+int main() {
+	// 变量
+	std::string words_to_output;
+	words_to_output = "Hello World.";
+	// 字符串 string
+	std::cout << words_to_output;
+	std::cin.get();
+}
